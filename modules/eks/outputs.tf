d0adbb971c907dfd8ac953565a635beb810b29e1
Cyberkabil10 <49335525+Cyberkabil10@users.noreply.github.com>
2025-09-19 12:46:36 +0100

    configure the argo env + some changes to eks module

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -2,6 +2,10 @@ output "cluster_name" {
   value = module.eks.cluster_name
 }
 
+output "cluster_id" {
+  value = module.eks.cluster_id
+}
+
 output "cluster_endpoint" {
   value = module.eks.cluster_endpoint
 }
