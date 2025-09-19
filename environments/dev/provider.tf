ca62f099b27acac6b25ce008d6cf058fef86c50d
Cyberkabil10 <49335525+Cyberkabil10@users.noreply.github.com>
2025-09-19 11:24:41 +0100

    Add dev environment files

---
+++ environments/dev/provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 6.8"
+    }
+  }
+
+  backend "remote" {
+    organization = "sobeam-project-supcom"
+
+    workspaces {
+      name = "sobeam-infrastructure-supcom-dev"
+    }
+  }
+}
