e4c268454bef4439afd2ee521f90dcad84bc75ab
Cyberkabil10 <49335525+Cyberkabil10@users.noreply.github.com>
2025-09-19 12:42:07 +0100

    add argocd config and configure isntallation

---
+++ environments/dev/argocdInstall.tf
@@ -0,0 +1,34 @@
+provider "kubectl" {
+  load_config_file       = false
+  host                   = data.aws_eks_cluster.dev-eks-cluster.endpoint
+  token                  = data.aws_eks_cluster_auth.dev-eks-cluster.token
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.dev-eks-cluster.certificate_authority.0.data)
+
+}
+
+data "kubectl_file_documents" "namespace" {
+  content = file("../argocd/manifests/namespace.yaml")
+}
+
+data "kubectl_file_documents" "argocd" {
+  content = file("../argocd/manifests/install.yaml")
+}
+
+resource "kubectl_manifest" "namespace" {
+  depends_on = [
+    module.eks.cluster_id
+  ]
+  count              = length(data.kubectl_file_documents.namespace.documents)
+  yaml_body          = element(data.kubectl_file_documents.namespace.documents, count.index)
+  override_namespace = "argocd"
+}
+
+resource "kubectl_manifest" "argocd" {
+  depends_on = [
+    kubectl_manifest.namespace
+  ]
+  count              = length(data.kubectl_file_documents.argocd.documents)
+  yaml_body          = element(data.kubectl_file_documents.argocd.documents, count.index)
+  override_namespace = "argocd"
+}
+
