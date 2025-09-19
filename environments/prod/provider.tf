85e51bb2834794ad2b20a60ec9c1917d58841074
Cyberkabil10 <49335525+Cyberkabil10@users.noreply.github.com>
2025-09-19 13:51:37 +0100

    postpone the remote backend

--- environments/prod/provider.tf
+++ environments/prod/provider.tf
@@ -6,11 +6,11 @@ terraform {
     }
   }
 
-  backend "remote" {
+  /*backend "remote" {
     organization = "sobeam-project-supcom"
 
     workspaces {
       name = "sobeam-infrastructure-supcom-prod"
     }
-  }
+  }*/
 }
